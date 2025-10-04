20dba48e69f5037b60a2d87d45d03b781421a699
Mustafa <mm0118250@gmail.com>
2025-10-04 14:52:24 +0300

    creating the src folder and pages and components forlder

---
+++ src/pages/components/GlobaleMessage.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function GlobaleMessage() {
+    return (
+        <View>
+            <Text>globaleMessage</Text>
+        </View>
+    );
+}
---
+++ src/pages/courses.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Courses() {
+    return (
+        <View>
+            <Text>courses</Text>
+        </View>
+    );
+}
